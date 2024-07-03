881ea8fb22775734268f4a0a395921fb5f829b42
asddfdf <468382010@qq.com>
2024-07-03 17:58:15 +0800

    add tailwindcss dependencies

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{vue,js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+} satisfies Config;
