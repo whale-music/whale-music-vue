1513c0d5e212ca2aa2c0ee69cab5026152b804ac
asddfdf <468382010@qq.com>
2024-07-04 16:19:42 +0800

    perf: html lang use i18n

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,16 +1,18 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n } from "vue-i18n";
 
-import zhCN from './lang/zh-CN.ts';
+import zhCN from "./lang/zh-CN.ts";
 
+function initCreateI18n() {
+  const locale = navigator.language.replace("-", "") ?? "zh-CN";
+  document.querySelector("html")?.setAttribute("lang", locale);
+  return createI18n({
+    locale: locale,
+    fallbackLocale: "en",
+    legacy: false,
+    messages: {
+      zhCN,
+    },
+  });
+}
 
-const i18n = createI18n({
-  locale: 'zhCN',
-  fallbackLocale: 'en',
-  legacy: false,
-  messages: {
-    zhCN
-  }
-})
-export  {
-  i18n
-};
+export const i18n = initCreateI18n();
