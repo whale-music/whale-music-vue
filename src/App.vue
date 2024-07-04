f27e250fe9ddd99f1d67d6542f163c4251ac4bef
asddfdf <468382010@qq.com>
2024-07-04 18:58:07 +0800

    feat: add music player drawer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-import TopNavigationBar from '@/components/TopNavigationBar/index.vue'
-import { useColorMode } from '@vueuse/core'
-import { ScrollArea } from '@/shadcn/components/ui/scroll-area'
-useColorMode()
+import TopNavigationBar from "@/components/TopNavigationBar/index.vue";
+import { useColorMode } from "@vueuse/core";
+import { ScrollArea } from "@/shadcn/components/ui/scroll-area";
+import MusicPlayer from "@/components/MusicPlayer/index.vue";
+import { Button } from "@/shadcn/components/ui/button";
+import { ref } from "vue";
+useColorMode();
+const value = ref(false);
 </script>
 
 <template>
@@ -11,9 +15,11 @@ useColorMode()
     <ScrollArea class="h-full w-full">
       <div class="custom-padding">
         <RouterView />
+        <Button @click="value = !value">open</Button>
       </div>
     </ScrollArea>
   </main>
+  <MusicPlayer v-model="value"/>
 </template>
 
 <style lang="css" scoped>
---
+++ src/components/MusicPlayer/index.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import {
+  MusicPlayerDrawer,
+  MusicPlayerDrawerContent,
+  MusicPlayerDrawerFooter,
+  MusicPlayerDrawerTitle,
+  MusicPlayerDrawerDescription,
+  DrawerClose,
+} from "@/components/MusicPlayer/components/MusicPlayerDrawer";
+import { Button } from "@/shadcn/components/ui/button";
+import MaterialSymbolsKeyboardArrowDownRounded from "@/assets/icon/MaterialSymbolsKeyboardArrowDownRounded.vue";
+
+defineOptions({
+  name: "MusicPlayer",
+});
+
+const value = defineModel({
+  default: false,
+});
+</script>
+
+<template>
+  <MusicPlayerDrawer v-model:open="value" :fade-from-index="9999">
+    <MusicPlayerDrawerContent>
+      <MusicPlayerDrawerTitle />
+      <MusicPlayerDrawerDescription>
+        <MusicPlayerDrawerFooter class="h-screen">
+          <DrawerClose class="icon-button-close">
+            <button class="hover:bg-accent rounded-lg">
+              <MaterialSymbolsKeyboardArrowDownRounded
+                class="w-10 h-10 text-foreground"
+              />
+            </button>
+          </DrawerClose>
+        </MusicPlayerDrawerFooter>
+      </MusicPlayerDrawerDescription>
+    </MusicPlayerDrawerContent>
+  </MusicPlayerDrawer>
+</template>
+
+<style lang="scss" scoped>
+.icon-button-close {
+  position: fixed;
+  top: 24px;
+  right: 24px;
+  z-index: 300;
+  border-radius: 0.75rem;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0.28;
+  transition: opacity 0.2s ease;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
