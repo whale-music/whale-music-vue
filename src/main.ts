192af8a1580c18198a55eb683fa1fbf975b6c632
asddfdf <468382010@qq.com>
2024-07-05 11:56:02 +0800

    feat: add pinia dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import '@/style/tailwind.css'
 import App from './App.vue'
 import { router } from '@/router'
 import { i18n } from '@/locale'
-
+import { store } from "@/store";
 
 createApp(App)
+    .use(store)
     .use(router)
     .use(i18n)
     .mount('#app')
