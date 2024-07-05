21f642d8c23a5d2dfe193ff40690c06c998949cc
asddfdf <468382010@qq.com>
2024-07-05 11:29:22 +0800

    feat: add icon

---
+++ src/assets/icon/LucideRepeat1.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path d="m17 2l4 4l-4 4"></path>
+      <path d="M3 11v-1a4 4 0 0 1 4-4h14M7 22l-4-4l4-4"></path>
+      <path d="M21 13v1a4 4 0 0 1-4 4H3m8-8h1v4"></path>
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: "LucideRepeat1",
+};
+</script>
