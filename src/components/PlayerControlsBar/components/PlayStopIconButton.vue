cc91be2fb9cfd9de86f98d261c06b072ddf6db50
asddfdf <468382010@qq.com>
2024-07-07 12:18:35 +0800

    feat: add play and stop icon button

---
+++ src/components/PlayerControlsBar/components/PlayStopIconButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconButton from "@/components/PlayerControlsBar/components/IconButton.vue";
+import SolarPlayBold from "@/assets/icon/SolarPlayBold.vue";
+import SolarStopCircleBold from "@/assets/icon/SolarStopCircleBold.vue";
+
+defineOptions({
+  name: "PlayStopIconButton",
+});
+const value = defineModel();
+</script>
+
+<template>
+  <IconButton class="mx-2">
+    <SolarStopCircleBold class="size-8 ml-1.5 mr-2 my-2" v-if="value" />
+    <SolarPlayBold class="size-8 ml-1.5 mr-2 my-2" v-else />
+  </IconButton>
+</template>
+
+<style scoped></style>
