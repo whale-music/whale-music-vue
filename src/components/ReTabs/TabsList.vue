6a22781b9a5c4fa85f054ee63ad988a5f8bde419
asddfdf <468382010@qq.com>
2024-07-11 10:36:40 +0800

    feat: add ReTabs components

---
+++ src/components/ReTabs/TabsList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { TabsList, type TabsListProps } from 'radix-vue'
+import { cn } from '@/shadcn/utils'
+
+const props = defineProps<TabsListProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <TabsList
+    v-bind="delegatedProps"
+    :class="cn(
+      'inline-flex h-9 items-center justify-center rounded-lg bg-background p-1 text-muted-foreground',
+      props.class,
+    )"
+  >
+    <slot />
+  </TabsList>
+</template>
---
+++ src/components/ReTabs/index.ts
@@ -0,0 +1,4 @@
+export { default as ReTabs } from './Tabs.vue'
+export { default as ReTabsTrigger } from './TabsTrigger.vue'
+export { default as ReTabsList } from './TabsList.vue'
+export { default as ReTabsContent } from './TabsContent.vue'
