8dee1ff294d2aa7fa3da56e04be7ebbf2fcce36e
asddfdf <468382010@qq.com>
2024-07-11 10:43:02 +0800

    feat: add browse tabs

--- src/page/BrowsePage/BrowsePage.vue
+++ src/page/BrowsePage/BrowsePage.vue
@@ -1,13 +1,59 @@
 <script setup lang="ts">
+import {
+  ReTabs,
+  ReTabsList,
+  ReTabsTrigger,
+  ReTabsContent,
+} from "@/components/ReTabs";
+import { useI18n } from "vue-i18n";
+
 defineOptions({
-  name: 'BrowsePage'
-})
+  name: "BrowsePage",
+});
+const { t } = useI18n();
+
+const tabsList = [
+  {
+    value: "playlist",
+    label: t("browse.playlist"),
+  },
+  {
+    value: "music",
+    label: t("browse.music"),
+  },
+  {
+    value: "album",
+    label: t("browse.album"),
+  },
+  {
+    value: "artist",
+    label: t("browse.artist"),
+  },
+  {
+    value: "MV",
+    label: t("browse.mv"),
+  },
+  {
+    value: "Tag",
+    label: t("browse.tag"),
+  },
+];
 </script>
 
 <template>
-  <h1>Browse Page</h1>
+  <div>
+    <h1 class="text-5xl font-semibold my-8">{{ $t("nav.browse") }}</h1>
+    <ReTabs default-value="playlist" class="w-[600px]">
+      <ReTabsList class="grid w-full grid-cols-6 my-4">
+        <ReTabsTrigger :value="i.value" v-for="i in tabsList" :key="i.value">
+          {{ i.label }}
+        </ReTabsTrigger>
+      </ReTabsList>
+      <ReTabsContent :value="i.value" v-for="i in tabsList" :key="i.value">
+        <h1>{{ i.label }}</h1>
+      </ReTabsContent>
+    </ReTabs>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
