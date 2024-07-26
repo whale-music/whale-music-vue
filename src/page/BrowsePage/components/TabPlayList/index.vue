ad04e9be2a4a68cd972ea5e2378dc9c70cf49e40
asddfdf <468382010@qq.com>
2024-07-26 10:33:59 +0800

    refactor: update img url

--- src/page/BrowsePage/components/TabPlayList/index.vue
+++ src/page/BrowsePage/components/TabPlayList/index.vue
@@ -10,7 +10,7 @@ defineOptions({
   <div class="grid sm:grid-cols-3 md:grid-cols-5 gap-x-6 gap-y-11">
     <CoverCard
       title="123123"
-      src="https://t.alcy.cc/fj/"
+      src="https://imgapi.xl0408.top/index.php"
       v-for="i in 100"
       :key="i"
     />
