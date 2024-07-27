b09cc02f4b8cf30417bf80a6591af05dc6c94150
asddfdf <468382010@qq.com>
2024-07-27 10:09:15 +0800

    feat: add language zh-CN en

---
+++ src/locale/lang/en.ts
@@ -0,0 +1,50 @@
+export default {
+  nav: {
+    home: "Home",
+    dashboard: "Dashboard",
+    browse: "Browse",
+    library: "Music Library",
+    search: "Search",
+  },
+  browse: {
+    playlist: "Playlist",
+    music: "Music",
+    album: "Album",
+    artist: "Artist",
+    mv: "MV",
+    tag: "Tag",
+  },
+  settings: {
+    settings: "Settings",
+    general: "General",
+    refresh: "Refresh",
+    logout: "Logout",
+    language: "Language",
+    musicQuality: {
+      text: "Music Quality",
+      low: "Low",
+      medium: "Medium",
+      high: "High",
+      lossless: "Lossless",
+    },
+    lyric: {
+      text: "Lyrics",
+    },
+    others: "Others",
+    appearance: {
+      text: "Appearance",
+      auto: "Auto",
+      light: "Light",
+      dark: "Dark",
+    },
+    performance: {
+      text: "Performance",
+      topBar: "Top Bar Blur",
+      bottomBar: "Bottom Bar Blur",
+      playInterface: {
+        text: "Play Interface",
+        default: "Default",
+      },
+    },
+  },
+};
--- src/locale/lang/zh-CN.ts
+++ src/locale/lang/zh-CN.ts
@@ -1,10 +1,10 @@
 export default {
   nav: {
-    home: '首页',
-    dashboard: '概况',
-    browse: '浏览',
-    library: '音乐库',
-    search: '搜索',
+    home: "首页",
+    dashboard: "概况",
+    browse: "浏览",
+    library: "音乐库",
+    search: "搜索",
   },
   browse: {
     playlist: "歌单",
@@ -15,19 +15,36 @@ export default {
     tag: "Tag",
   },
   settings: {
-    settings: '设置',
-    refresh: '刷新',
-    logout: '登出',
-    language: '语言',
+    settings: "设置",
+    general: "通用",
+    refresh: "刷新",
+    logout: "登出",
+    language: "语言",
+    musicQuality: {
+      text: "音质",
+      low: "普通",
+      medium: "较高",
+      high: "极高",
+      lossless: "无损",
+    },
     lyric: {
-      text: '歌词',
+      text: "歌词",
     },
-    others: '其他',
+    others: "其他",
     appearance: {
-      text: '外观',
-      auto: '自动',
-      light: '浅色',
-      dark: '深色',
+      text: "外观",
+      auto: "自动",
+      light: "浅色",
+      dark: "深色",
     },
-  }
+    performance: {
+      text: "性能",
+      topBar: "顶栏模糊",
+      bottomBar: "底栏模糊",
+      playInterface: {
+        text: "播放界面",
+        default: "默认",
+      },
+    },
+  },
 };
